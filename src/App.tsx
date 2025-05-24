8b113ba00da331d6c6cd52ad765467e2641e9731
Thibault Lombart <lombart.thibault@gmail.com>
2025-05-24 04:44:08 +0200

    adding archives

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import './App.css';
 import Map from "./pages/MapPage"
+import Films from "./pages/Films";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/map" element={<Map />} />
+                <Route path="/archives" element={<Films />} />
             </Routes>
         </Router>
     );
