5ebb52ecd92c46271d1062365348683bc1e8f54c
Mattéo Cuisset <81548033+Flyns157@users.noreply.github.com>
2025-05-24 05:12:24 +0200

    Revert "Features/carte"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
+import "./App.css";
 import HeaderComponent from "./components/header/HeaderComponent";
-import Map from "./pages/MapPage"
-import Films from "./pages/Films";
-import ActorsPage from "./pages/ActorsPage";
 
 const App = () => {
-
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
@@ -19,9 +15,6 @@ const App = () => {
             path="/"
             element={<Home isOpen={isOpen} setIsOpen={setIsOpen} />}
           />
-            <Route path="/map" element={<Map />} />
-            <Route path="/actors" element={<ActorsPage />} />
-            <Route path="/archives" element={<Films />} />
         </Routes>
       </Router>
     </>
