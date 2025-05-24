d057c8089d4d94b7e8bb087fad6cf07a1375d770
Thibault Lombart <lombart.thibault@gmail.com>
2025-05-24 03:29:09 +0200

    suite

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import './App.css';
+import Map from "./pages/MapPage"
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/map" element={<Map />} />
             </Routes>
         </Router>
     );
--- src/components/arrows/NextArrow.tsx
+++ src/components/arrows/NextArrow.tsx
@@ -4,13 +4,13 @@ type NextButtonProps = {
     children: string;
 };
 
-export default function NextButton({ children }:  NextButtonProps) {
+export default function NextArrow({ children }:  NextButtonProps) {
     const navigate = useNavigate();
 
     return (
         <button
             onClick={() => navigate(children)}
-            className="fixed right-4 top-1/2 transform -translate-y-1/2 bg-white/10 border-2 border-white text-white text-2xl p-4 rounded-full backdrop-blur z-50 hover:bg-white/20 transition"
+            className="fixed right-6 bottom-8 transform -translate-y-1/2 bg-white/10 border-2 border-white text-white text-2xl p-4 rounded-full backdrop-blur z-50 hover:bg-white/20 transition"
             aria-label="Page suivante"
         >
             →
