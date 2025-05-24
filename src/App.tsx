5e0e1c24b3e4046ebfed2b981b74420c093b4d05
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2025-05-24 04:00:13 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import './App.css';
+import "./App.css";
+import HeaderComponent from "./components/header/HeaderComponent";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <HeaderComponent isOpen={isOpen} />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home isOpen={isOpen} setIsOpen={setIsOpen} />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
